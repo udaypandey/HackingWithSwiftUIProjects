3d6b8054cdfee8c3dc1de40dc6c3abf4bb9bf0ec
Uday Pandey <uday.pandey@crvsh.io>
2020-07-16 06:39:54 +0100

    Done WeSplit project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Uday Pandey on 16/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
