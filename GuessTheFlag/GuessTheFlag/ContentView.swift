6a0100c844803a70a61d12a5ecc060eb4147e583
Uday Pandey <uday.pandey@crvsh.io>
2020-07-25 17:32:49 +0100

    Finished Guess the flag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+
+    @State private var selected  = ""
+    @State private var showAnswerAlert  = false
+    @State private var alertTitle  = ""
+
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            ZStack {
+                Color.gray
+
+                VStack {
+                    Text("Tell me the flag for \(selected)")
+                        .foregroundColor(.white)
+
+                    VStack(spacing: 50) {
+                        ForEach(countries[0..<3], id: \.self) { country in
+                            Button(action: {
+                                checkAnswer(country: country)
+                            }) {
+                                Image(country)
+                                    .renderingMode(.original)
+                                    .clipShape(Capsule())
+                            }
+                        }
+
+                        Spacer()
+                    }
+                    .onAppear {
+                        self.reset()
+                    }
+                }
+            }
+            .alert(isPresented: $showAnswerAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertTitle),
+                      dismissButton: .cancel(Text("Ok"), action: {
+                        self.reset()
+                      }))
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationTitle("Guess Flag")
+        }
     }
+
+    private func reset() {
+        countries.shuffle()
+        selected = countries[Int.random(in: 0..<2)]
+    }
+
+    private func checkAnswer(country: String) {
+        alertTitle = (country == selected) ? "Correct" : "Incorrect"
+        showAnswerAlert = true
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
