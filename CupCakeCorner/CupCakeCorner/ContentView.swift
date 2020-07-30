dd5c5bcb51ce543630f92d857b49c8d98a266549
Uday Pandey <uday.pandey@crvsh.io>
2020-07-30 16:31:09 +0100

    all changes

--- CupCakeCorner/CupCakeCorner/ContentView.swift
+++ CupCakeCorner/CupCakeCorner/ContentView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cakeType = 0
+    @State private var numberOfCakes = 0
+
+    @State private var specialRequests = false
+    @State private var extraFrosting = false
+    @State private var extraSprinkles = false
+
+    let cakeTypes = [
+        "Vanilla",
+        "Strawberry",
+        "Choclate",
+        "Rainbow"
+    ]
+
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $cakeType) {
+                        ForEach(cakeTypes, id: \.self) { cupCake in
+                            Text(cupCake)
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(cakeType)", value: $cakeType)
+                }
+
+                Section {
+                    Toggle("Any special requests", isOn: $specialRequests.animation())
+
+                    if specialRequests {
+                        Toggle("Add extra frostings", isOn: $extraFrosting)
+
+                        Toggle("Add extra sprinkles", isOn: $extraSprinkles)
+                    }
+                }
+
+                Section {
+                    NavigationLink(
+                        destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/,
+                        label: {
+                            Text("Delivery Details")
+                        })
+                }
+
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
 }
 
